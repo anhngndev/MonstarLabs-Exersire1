70b4746863258fc5f8bcd02dff140b49dc35a2db
Anhngndev <anhngn296@gmail.com>
2021-04-08 17:47:48 +0700

    ok

---
+++ app/src/main/java/com/example/mljvc_temp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mljvc_temp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        Giờ tôi có 1 project mới tinh nè. Tôi muốn tạo thêm 1 branch ở repo kia và push proj này lên đó,
+    }
+}
